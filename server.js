345996eb4f172bbe33f18f5e0949972c90c3d4ff
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-06-11 03:27:40 +0100

    awakening

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const post_route = require('./routes/postRoute')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const post_route = require("./routes/postRoute");
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/home', (req, res)=> {
-    res.send('SQL home')
-})
-app.post('/blog', post_route)
+app.get("/home", (req, res) => {
+  res.send("SQL home");
+});
+app.post("/blog", post_route);
 
-app.listen(6060, ()=>{
-    console.log('running')
-})
+const PORT = process.env.PORT || 9000;
 
+try {
+  app.listen(PORT, () => {
+    console.log("running");
+  });
+} catch (err) {
+  console.log(err);
+}
